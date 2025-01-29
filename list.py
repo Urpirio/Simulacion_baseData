e2bc58c5897e761d4c6a881e230661f5d5051e96
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-29 19:49:28 -0400

    Simulación de una base de datos.

---
+++ list.py
@@ -0,0 +1,35 @@
+listCar = [{
+    "CarName": "Camaro",
+    "Marca": "Chevrolet",
+    "Anio": 2018
+    },
+    {
+    "CarName": "Mustang",
+    "Marca": "Ford",
+    "Anio": 2023
+    },
+    {
+    "CarName": "Veloster",
+    "Marca": "Hyunday",
+    "Anio": 2019
+    },
+    {
+    "CarName": "Sonata",
+    "Marca": "Hyunday",
+    "Anio": 2017
+    }]
+search = input("Que buscas?")
+if search == "Camaro":
+    print(listCar[0])
+elif search == "Mustang":
+    print(listCar[1])
+elif search == "Veloster":
+    print(search[2])
+elif search == "Sonata":
+    print(search[3])
+else:
+    print("No existe tal modelo de auto")
+ 
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
